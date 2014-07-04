96bbd8a3f451fd059498950f25390035d526a1a2
Alan <alin@2u.com>
2014-07-04 18:58:07 +0000

    first commit

---
+++ schedule.js
@@ -0,0 +1,12 @@
+
+
+// /repos/:owner/:repo/commits
+
+// https://api.github.com/users/linstantnoodles/repos
+
+// Get all user repos
+// Sort by last 5 recently updated
+// Get all commits of these 5
+// Sort by # of commits
+// Pop off the top
+// Get punchcard
